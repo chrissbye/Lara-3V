3916d5ea60c27814cfd55b48ae9806bece92b057
chrissbye <FINNBBG89@gmail.com>
2025-11-11 23:03:47 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "rdA2FKYS#MB9o3FQ9-dr3GB5MLe4ObT5sPXrZ-dYc83F9kc7dX_g", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
